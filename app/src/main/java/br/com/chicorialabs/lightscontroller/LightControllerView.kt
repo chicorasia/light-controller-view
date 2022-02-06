85ac76bb692a2a8c550330e4ac8fcd65a8b2af10
chicorasia <chicorasia@gmail.com>
2022-02-06 07:50:55 -0300

    Initial commit - project creation

---
+++ app/src/main/java/br/com/chicorialabs/lightscontroller/LightControllerView.kt
@@ -0,0 +1,20 @@
+package br.com.chicorialabs.lightscontroller
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Essa classe representa uma View personalizada que herda
+ * diretamente da classe View.
+ */
+class LightControllerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    //..Implemente a view aqui...
+
+
+}
